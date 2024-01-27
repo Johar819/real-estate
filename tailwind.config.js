178e763c0324c7cfb576b6156ff9db0cc9750046
Waquar Ahmad <awaquar819@gmail.com>
2024-01-27 23:09:33 +0530

    Install react js and tailwind css ans create the first template

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+/*eslint-env node*/
+export default {
+  content: [
+    "./src/**/*.{html, js, ts, vue}", 
+    "./src/**/*"
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
