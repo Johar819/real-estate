6bf750b551491712c7fa66832fb2b646e354835e
Waquar Ahmad <awaquar819@gmail.com>
2024-01-28 15:28:08 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+dotenv.config();
+mongoose.connect(process.env.MONGODB_URL, {})
+  .then(() => {
+    console.log("Database Connection Established Successfully");
+  })
+  .catch((err) => {
+    console.log("Database Connection Failed");
+    console.error(err);
+    process.exit(1);
+  });
 const app = express();
-app.listen(3000,()=>{
-    console.log("Server is running at port 3000!");
-})
+app.listen(3000, () => {
+  console.log("Server is running at port 3000!");
+});
