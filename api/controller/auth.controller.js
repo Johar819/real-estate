2730a147c6585ed1030da0c88e8c55496c8b4138
Waquar Ahmad <awaquar819@gmail.com>
2024-01-28 23:44:33 +0530

    create sign in api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,5 +1,9 @@
 const User = require('../models/user.model.js')
 const bcryptjs = require('bcryptjs');
+const { errorHandler } = require('../utils/error.js');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
 exports.signup = async (req,res,next)=>{
     const {username,email,password} = req.body;
     const hashedPassword =  bcryptjs.hashSync(password,10);
@@ -18,4 +22,30 @@ exports.signup = async (req,res,next)=>{
     }
 }
 
-exports.signin = (req,res)=>{}
+exports.signin = async (req,res,next)=>{
+    const {email,password} = req.body;
+    try{
+        const validUser = await User.findOne({email});
+        if(!validUser){
+            return next(errorHandler(404,"User not found"));
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if(!validPassword){
+            return next(errorHandler(401,"Incorrect Password"));
+        }
+        const {password:pass,...others} = validUser._doc;
+        const token = jwt.sign({
+            id:validUser._id,
+        },process.env.JWT_SECRET,{
+            expiresIn:"1d"
+        });
+        res.cookie("access_token",token,{    
+            httpOnly:true,
+        }).status(200).json({
+            others
+        });
+        
+    }catch(err){
+        next(err);
+    }
+}
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { signup } = require("../controller/auth.controller.js");
+const { signup, signin } = require("../controller/auth.controller.js");
 const router  = express.Router();
 router.post('/signup',signup)
+router.post('/signin',signin)
 module.exports = router;
