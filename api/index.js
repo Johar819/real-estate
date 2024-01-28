1219e95927059e3a427fadc0af56955ff9c50ca2
Waquar Ahmad <awaquar819@gmail.com>
2024-01-28 16:08:00 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require('dotenv');
+const userRouter = require('./routes/user.route')
 dotenv.config();
 mongoose.connect(process.env.MONGODB_URL, {})
   .then(() => {
@@ -15,3 +16,4 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running at port 3000!");
 });
+app.use('/api/user',userRouter);
---
+++ api/routes/user.route.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router  = express.Router();
+const {test} = require("../controller/user.controller")
+router.get('/test',test)
+module.exports = router;
