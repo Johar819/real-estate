b26a4567b7e810825badd38190370a1ad88858fd
Waquar Ahmad <awaquar819@gmail.com>
2024-08-16 23:38:15 +0530

    jwt error in veriryUser.js

--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,16 +1,15 @@
-import { errorHandler } from "./error.js";
-import { verify } from "jsonwebtoken";
-export async function verifyUser(req,res,next){    
-        const token = req.cookies.access_token;
-        if(!token){
-          return next(errorHandler(401, "Please login first"))
-        }
-        verify(token, process.env.JWT_SECRET,(err,user)=>{
-            if(err){
-                return next(errorHandler(403, "Please login again"))
-            }
-            // console.log("User",user);
-            req.user = user;
-            next();
-        });
-}
+import jwt from 'jsonwebtoken';
+import { errorHandler } from './error.js';
+
+export const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+
+  if (!token) return next(errorHandler(401, 'Unauthorized'));
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(errorHandler(403, 'Forbidden'));
+
+    req.user = user;
+    next();
+  });
+};
