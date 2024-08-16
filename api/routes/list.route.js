4a9e9ed7590483aa38c6946f1d0ccae48f42aa63
Waquar Ahmad <awaquar819@gmail.com>
2024-08-17 00:18:54 +0530

    fixed

--- api/routes/list.route.js
+++ api/routes/list.route.js
@@ -1,10 +1,10 @@
 import { Router as _Router } from "express";
 const Router = _Router();
 import { createList, deleteList, updateList, getLists, getList } from "../controller/list.controller.js";
-import { verifyUser } from "../utils/verifyUser.js";
-Router.post("/create",verifyUser,createList);
-Router.delete('/delete/:id',verifyUser,deleteList);
-Router.post('/update/:id',verifyUser,updateList);
+import { verifyToken } from "../utils/verifyUser.js";
+Router.post("/create",verifyToken,createList);
+Router.delete('/delete/:id',verifyToken,deleteList);
+Router.post('/update/:id',verifyToken,updateList);
 Router.get('/get/:id',getList);
 Router.get('/get',getLists);
 export default Router
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -3,8 +3,8 @@ const router  = Router();
 import { test, updateUser,deleteUser, getUserLists, getUser } from "../controller/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 router.get('/test',test)
-router.post('/update/:id',verifyUser,updateUser);
+router.post('/update/:id',verifyToken,updateUser);
 router.delete('/delete/:id',verifyToken,deleteUser);
-router.get('/listings/:id',verifyUser,getUserLists);
-router.get('/:id',verifyUser,getUser);
+router.get('/listings/:id',verifyToken,getUserLists);
+router.get('/:id',verifyToken,getUser);
 export default router;
