fff0555b2758f22c697596b689750ca247963818
Waquar Ahmad <awaquar819@gmail.com>
2024-08-16 22:13:24 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ const userRouter = require("./routes/user.route.js");
 const authRouter = require("./routes/auth.route.js");
 const listRouter = require("./routes/list.route.js");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 dotenv.config();
 mongoose
   .connect(process.env.MONGODB_URL, {})
@@ -16,12 +17,20 @@ mongoose
     console.error(err);
     process.exit(1);
   });
+
+const __dirname = path.resolve();
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/list", listRouter);
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
+
 app.use((err, req, res, next) => {
   const status = err.statusCode || 500;
   const message = err.message || "Something went wrong!";
